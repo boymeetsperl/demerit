5204b2c7e018c814d7e43abe28c53e54061cd17b
Ian McGaunn <ianmcgaunn@icloud.com>
2014-01-25 11:41:52 -0500

    added base function to run a program given its path

--- demerit/eval.py
+++ demerit/eval.py
@@ -1,4 +1,18 @@
 import shared
+from subprocess import call, check_output
 
 # this will contain the functions relevant to evaluating
 # student programs.
+
+# function to run program, maps student name to output from program
+# or none, if the program did not successfully run or produce output
+
+# returns output from running program, or None if unsuccessful
+def run_program(path):
+    try:
+        output = check_output(path)
+    except CalledProcessError, e:
+        print(e.returncode)
+        output = None
+
+    return output
