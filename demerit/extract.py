33a41a370d9fd1c725d7c4c0449875ec6cc04515
Ian McGaunn <ianmcgaunn@icloud.com>
2014-01-25 10:41:12 -0500

    removed extra print statements for readability

--- demerit/extract.py
+++ demerit/extract.py
@@ -7,14 +7,11 @@ def extract_assign(user_map):
     assign_dir_map = {}
 
     for key in user_map:
-        print key
         curr_dir = shared.GRADE_DIR + "/{}".format(key)
         curr_archive = shared.GRADE_DIR + "/{}".format(user_map[key])
         if path.exists(curr_dir) != True:
             mkdir(curr_dir)
 
-        print("current file: {}".format(curr_archive))
-        print("current directory: {}".format(curr_dir))
         status = call(["tar", "-xf", curr_archive,
                       "--directory", curr_dir])
         if status != 0:
