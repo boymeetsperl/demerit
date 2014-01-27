cd48d87f58b4392acbba5991aa18d39ac442b0b9
Ian McGaunn <ianmcgaunn@icloud.com>
2014-01-27 11:55:39 -0500

    added function to run all programs and record results

--- demerit/eval.py
+++ demerit/eval.py
@@ -14,14 +14,31 @@ def run_program(path):
     arg_list.append(path)
 
     if shared.config["argv"] != None:
-        arg_list.append(shared.ARGV)
-
-    print arg_list
+        arg_list.append(shared.config["argv"])
 
     try:
         prog = Popen(arg_list, stdout=PIPE, stdin=PIPE, stderr=shared.DEVNULL)
-        output = prog.communicate(input=shared.config["stdin"])[0]
+        if shared.config["stdin"] != False:
+            output, error = prog.communicate(input=shared.config["stdin"])
+        else:
+            output, error = prog.communicate()
     except CalledProcessError:
+        print "failed"
         output = None
-
     return output
+
+def run_all(executable_map):
+    user_output_map = {}
+
+    for student in executable_map:
+        if executable_map[student] != None:
+            output = run_program(executable_map[student])
+            user_output_map[student] = output
+        else:
+            user_output_map[student] = None
+
+    return user_output_map
+
+def check_program(user, executable):
+    # attempt to autoeval. if unsuccessful, ask me about it
+    print("checking {}".format(user))
