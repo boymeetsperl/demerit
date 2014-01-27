e9ed31986732bd4204c5db4b19bef9fb41766b09
Ian McGaunn <ianmcgaunn@icloud.com>
2014-01-27 08:59:32 -0500

    further simplified loading of configuration options

--- demerit/shared.py
+++ demerit/shared.py
@@ -1,16 +1,14 @@
+import copy
 from os import getcwd
 from yaml import load, dump
 
 try:
-    CONFIG = open(getcwd()+"/"+"config.yml", "r")
+    CONFIG_FILE = open(getcwd()+"/"+"config.yml", "r")
 except IOError:
     print "can't load config"
     sys.exit(0)
 
-opts = load(CONFIG)
-
-config = {}
-for key in opts:
-    config[key] = opts[key]
+opts = load(CONFIG_FILE)
+config = copy.deepcopy(opts)
 
 DEVNULL = open("/dev/null", "w")
